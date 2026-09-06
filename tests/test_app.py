7a5cde5121df962c8e4a2d3dd1a67ad98935c5b5
kshithi18965 <kshithilpadav@gmail.com>
2026-09-06 03:50:32 +0000

    fix: resolve pytest import issue + simplify docker build

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# add project root to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from main import greet
 
 def test_greet_success():
